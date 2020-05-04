76ca7b76ea9a07cc28b88bbf564dc09720f38c74
maxdexter <maxdexter@bk.ru>
2020-05-04 17:52:45 +0300

    создал абстрактный класс BaseActivity для сохранения ичтения настроек приложения

--- app/src/main/java/com/maxdexter/liteweather/tools/BaseActivity.java
+++ app/src/main/java/com/maxdexter/liteweather/tools/BaseActivity.java
@@ -7,16 +7,35 @@ import android.os.PersistableBundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.maxdexter.liteweather.R;
+
 public abstract class BaseActivity extends AppCompatActivity {
     private static final String sNameSharedPreference = "LOGIN";
+    //Имя параметра в настройках
     private static final String sISDarkThem = "IS_DARK_THEM";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
-
+            if(isDarkTheme()){
+                setTheme(R.style.Theme_AppCompat_DayNight_NoActionBar);
+            } else {
+                setTheme(R.style.MyTheme);
+            }
 
     }
+    //Чтенеие настроек
+    protected boolean isDarkTheme(){
+        SharedPreferences sharedPref = getSharedPreferences(sNameSharedPreference, MODE_PRIVATE);
+        // Если настройка не найдена , то берется параметр по умолчанию
+        return sharedPref.getBoolean(sISDarkThem,true);
+    }
 
-   
-}
+    //Сохранение настроек
+    protected void setDarkTheme(boolean isDarkTheme){
+        SharedPreferences sharedPreferences = getSharedPreferences(sNameSharedPreference,MODE_PRIVATE);
+        //Параметры сохраняются посредствам специального класса editor
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(sISDarkThem,isDarkTheme);
+        editor.apply();
+    }}
