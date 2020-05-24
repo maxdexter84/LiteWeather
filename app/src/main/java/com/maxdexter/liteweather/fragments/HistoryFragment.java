3ac1a37f9cdf913eb09bafe66c8897d66fba7e87
maxdexter <maxdexter@bk.ru>
2020-05-24 14:14:12 +0300

    настроил удаление свайпом в лево

--- app/src/main/java/com/maxdexter/liteweather/fragments/HistoryFragment.java
+++ app/src/main/java/com/maxdexter/liteweather/fragments/HistoryFragment.java
@@ -1,10 +1,14 @@
 package com.maxdexter.liteweather.fragments;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
@@ -13,6 +17,7 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -22,6 +27,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 import com.maxdexter.liteweather.MainActivity;
 import com.maxdexter.liteweather.R;
 import com.maxdexter.liteweather.adapter.HistoryAdapter;
@@ -38,14 +45,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HistoryFragment extends BottomSheetDialogFragment {
+    ConstraintLayout mConstraintLayout;
     private List<HistoryWeather> mWeatherList;
     private static final int ID_DELETE = 10;
-
-
+    private HistoryAdapter historyAdapter;
+    private RecyclerView recyclerView;
     public static HistoryFragment newInstance(){
        return new HistoryFragment();
    }
@@ -59,14 +69,14 @@ public class HistoryFragment extends BottomSheetDialogFragment {
         View view = inflater.inflate(R.layout.fragment_history,container,false);
         initList();
         initRecyclerAdapter(view);
-
+        mConstraintLayout = view.findViewById(R.id.history_frag);
 
         return view;
     }
 
     private void initRecyclerAdapter(View view) {
-        RecyclerView recyclerView = view.findViewById(R.id.history_recycler);
-        HistoryAdapter historyAdapter = new HistoryAdapter(mWeatherList);
+        recyclerView = view.findViewById(R.id.history_recycler);
+        historyAdapter = new HistoryAdapter(mWeatherList);
         historyAdapter.setOnItemClickListener(new HistoryAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View v,String city) {
@@ -83,7 +93,10 @@ public class HistoryFragment extends BottomSheetDialogFragment {
     private void initList() {
        mWeatherList = HistoryBox.get(getActivity()).getHistoryWeatherList();
     }
-    private ItemTouchHelper.SimpleCallback mSimpleCallback = new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT) {
+    //создаем ItemTouchHelper.SimpleCallback для того что бы иметь возможность свайпать элементы списка или передвигать их и менять местами
+    //В качестве аргументов передаем 0,ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT
+    //0 значит что перетаскивать мы не будем, ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT значит что мы сможем свайпать как с лева на право так и с права на лево
+    private ItemTouchHelper.SimpleCallback mSimpleCallback = new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT) {//ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT
         @Override
         public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
             return false;
@@ -91,7 +104,27 @@ public class HistoryFragment extends BottomSheetDialogFragment {
 
         @Override
         public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            final int position = viewHolder.getAbsoluteAdapterPosition();// выясняем позицию элемента в адаптере
+            final HistoryWeather historyWeather = mWeatherList.get(position);
+            switch (direction){// через свич выясняем напраление свайпа и в зависимости от этого назначаем действие
+                case ItemTouchHelper.LEFT:
+                    HistoryBox.get(getContext()).deleteHistoryWeather(historyWeather);//Удаляем выбранный объект из базы данных
+                    mWeatherList.remove(position);// удаляем из списка
+                    historyAdapter.notifyItemRemoved(position);//обновляем адаптер
+                    break;
 
+            }
+
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addSwipeLeftBackgroundColor(ContextCompat.getColor(getContext(),R.color.red))
+                    .addSwipeLeftActionIcon(R.drawable.delete_icon)
+                    .create()
+                    .decorate();
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
         }
     };
 
